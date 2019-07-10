faee3921f9694e29df70b47cf3611c977b019d1a
YoucefSklab <ysklab@hotmail.com>
2019-07-10 10:40:51 +0200

    fix mini camera position

--- Assets/GamaSceneManagingScript/UI/MiniMapCamera.cs
+++ Assets/GamaSceneManagingScript/UI/MiniMapCamera.cs
@@ -1,22 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using ummisco.gama.unity.littosim;
 using UnityEngine;
 
 public class MiniMapCamera : MonoBehaviour {
     public Camera miniMapCamera;
     public Camera mainCamera;
+    private Canvas uiCanvas;
+    private GameObject uiManager;
+
+
     // Use this for initialization
     void Start () {
-		
-	}
+        uiManager = GameObject.Find("UIManager");
+        uiCanvas = GameObject.Find("Main_Canvas").GetComponent<Canvas>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-      
-        miniMapCamera.transform.localPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.localPosition.z);
-     //   miniMapCamera.GetComponent<RectTransform>().anchoredPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
+        miniMapCamera.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.localPosition.z);
+        // miniMapCamera.GetComponent<RectTransform>().anchoredPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
+        // miniMapCamera.transform.localPosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.localPosition.z));
+        miniMapCamera.transform.position = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -800));
+        Vector3 p = uiManager.GetComponent<UIManager>().worldToUISpace(uiCanvas, Input.mousePosition);
 
-      //  miniMapCamera.transform.localPosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.localPosition.z));
+        miniMapCamera.transform.position = new Vector3(p.x, p.y, -800);
 
     }
 }
